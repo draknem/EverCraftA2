6980fcd15bccca0e570b8afe2cdd889fa9f25b24
Draknem Flishkin <draknem@gmail.com>
2019-08-17 13:14:15 +0200

    Graphical fix
    Repainted the tiles, fixed tile gaps, created dice animations and object.

---
+++ Assets/Resources/Scripts/FollowerDice.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowerDice : MonoBehaviour
+{
+    public Transform hero;
+    public Transform dice;
+    public float voffset = 0.35f;
+    public Camera maincam;
+
+    void FixedUpdate()
+    {
+        Vector3 offset = new Vector3 (0f, maincam.orthographicSize/2 + voffset, 0f);
+        // /2 +0.35f, 0f);
+        dice.SetPositionAndRotation(hero.position+offset, dice.rotation);
+    }
+}
